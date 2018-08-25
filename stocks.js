5b69ede2d62232d2153b2e6be73e472d0653a7be
Thomas Russell <thomasbeckrussell@gmail.com>
2018-08-25 12:37:22 -0500

    Added Wiki API and Graph js libraries

--- stocks.js
+++ stocks.js
@@ -3,8 +3,13 @@ const newsKey = '72f9d35e3fde4f82b0de3f8e0996280a';
 const newsURL = 'https://newsapi.org/v2/top-headlines';
 const symbolURL = 'https://api.iextrading.com/1.0/ref-data/symbols';
 const stockURL = 'https://www.alphavantage.co/query';
-const symList = [];
-
+const wikiURL = 'https://en.wikipedia.org/w/api.php';
+const stockDay = {json: 'Time Series (Daily)', path: 'TIME_SERIES_DAILY'};
+const stockWeek = {json:'Weekly Time Series',path: 'TIME_SERIES_WEEKLY'};
+const stockMonth = {json: 'Monthly Time Series',path:'TIME_SERIES_MONTHLY'};
+const symList = new Object;
+let initData = [123,34,45,45,46,56,57,5,45,356,45,6,67,56];
+let myChart;
 
 function getToday(){
 	let date = new Date();
@@ -18,80 +23,210 @@ function getToday(){
 	if(mm<10){
 		mm = '0'+mm;
 	}
-	today = `${yyyy}-${mm}-${dd}`
+	let today = `${yyyy}-${mm}-${dd}`
 	return today;
 }
-function renderDashboard(sym,current,low,high){
+
+function renderWiki(data){
 	let html = `
-				<h1 class='big-symbol col-12'>${sym}</h1>
-                <h2>${current}</h2>
-                <p>Current Price</p>
-                <h2>${low}</h2>
+			<img class = "company-img" src=${data.thumbnail.source} alt="company pic?"/>
+                <p class="company-about">
+					${data.extract}
+                </p>
+	`;
+	$('.company-about').html(html);
+}
+
+function renderNews(data){
+	let html = `
+		${data.articles};
+	`;
+	$('.company-news').html(html);
+}
+
+function renderTable(sym,todayData){
+	let html = `
+				<h1 class='current-symbol'>${sym}</h1>
+                <h2 class="current-price">${todayData.close}</h2>
+                <h2>${todayData.tlow}</h2>
                 <p>52 Week Low</p>
-                <h2>${high}</h2>
+                <h2>${todayData.thigh}</h2>
                 </p>52 Week High</p>
 	`
-	$('.big-text').html(html);
+	$('.price-text').html(html);
 }
 
 function getStock(q){
 	let query = {
-		function: 'TIME_SERIES_WEEKLY',
 		symbol: q,
-		outputsize: 'compact',
+		function: stockWeek.path,
+		outputsize: 'full',
 		apikey: stockKey
 	};
 	$.getJSON(stockURL,query,function(data){
-		console.log(data['Weekly Time Series']);
-		let todayData = data['Weekly Time Series'][getToday()];
-		let close = todayData['4. close'];
-		let high = todayData['2. high'];
-		let low = todayData['3. low'];
+		console.log(data);
+		let fullData = data[stockWeek.json];
+		let arrayData = [];
 
-		let data52 = data['Weekly Time Series']
-		Object.keys(data52).forEach(function(key){
-			//console.log(key);
+		Object.keys(fullData).map(function(key){
+			let obj = new Object;
+			obj = fixKeys(fullData[key]);
+			obj['date'] = key;
+			arrayData.push(obj);
 		});
+		let todayData = arrayData[0];
 
+		//get 52 week high and lows
+		let arrayLow = [];
+		let arrayHigh = [];
+		let arrayVol = 0;
+		for (let i=0;i<52 && i<arrayData.length;i++){
+			arrayLow.push(parseInt(arrayData[i].low));
+			arrayHigh.push(parseInt(arrayData[i].high));
+			arrayVol += parseInt(arrayData[i].volume);
+		}
+		todayData['tlow'] = Math.min.apply(null,arrayLow);
+		todayData['thigh'] = Math.max.apply(null,arrayLow);
+		todayData['tVol'] = arrayVol/arrayData.length;
 
-		renderDashboard(q,close,low,high);
+		console.log(arrayData);
+		let graphLabels = ["one","two","three"];
+		let graphData = [1,2,3];
+		renderTable(q,todayData);
+		renderGraph(myChart,graphLabels,graphData);
 	});
 }
 
 function getNews(q){
 	let query = {
-		q: q,
-		apiKey: newsKey
+		apiKey: newsKey,
+		country: 'us',
+		category: 'technology'
 	};
 
+	if(q in symList) {
+        query['q'] = symList[q].split(' ')[0];
+    }
+	else{
+		query['q'] = q
+	}
+	query['q'] = 'google';
 	$.getJSON(newsURL,query,function(data){
-		console.log(data);
+		renderNews(data);
 
+	});
+}
+
+function getWiki(q){
+	let query = {
+		origin: '*',
+		action: 'query',
+		format: 'json',
+		prop: 'extracts|pageimages',
+		indexpageids: 1,
+		redirects: 1,
+		exchars: 1200,
+		// explaintext: 1,
+		exsectionformat: 'plain',
+		piprop: 'name|thumbnail|original',
+		pithumbsize: 250,
+		titles: q
+
+	}
+	$.getJSON(wikiURL,query,function(data){
+		console.log(data);
+		let wikiObj = data.query.pages[data.query.pageids[0]];
+		wikiObj['url'] = "https://en.wikipedia.org/wiki/";
+		renderWiki(wikiObj);
 	});
 }
 
 function handleSearch(){
 	$('.search-form').on('submit',(function(event){
 		event.preventDefault();
-		let userSearch = $('.search-bar').val();
-		getStock(userSearch);
-		getNews(userSearch);
+		let query = $('.search-bar').val();
+		let userSearch = matchSymbol(query);
+		getStock(userSearch.symbol);
+		getNews(userSearch.name);
+		getWiki(userSearch.name);
 	}))
+}
+
+function handleTime(){
+	$('.time-button').on('click',(function(event){
+		console.log($(this).text());
+	}))
+}
+
+function fixKeys(obj){
+	Object.keys(obj).forEach(function(key){
+		let newName = key.split('.')[1].trim();
+		obj[newName] = obj[key];
+		delete obj[key];
+	});
+	return obj;
+}
+
+function matchSymbol(query){
+	let returnObj = new Object;
+	query = query.toUpperCase();
+	if(query in symList){
+		returnObj['symbol'] = query;
+		returnObj['name'] = symList[query];
+	}
+	return returnObj;
 }
 
 function getSymbols(lis){
 	$.getJSON(symbolURL,{},function(data){
 		data.forEach(function(ele){
-			let obj = new Object;
-			obj[ele.symbol] = ele.name;
-			lis.push(obj);
+			lis[ele.symbol] = ele.name;
 		});
 	console.log(lis);
 	});
 }
 
+function renderGraph(chart, label, data) {
+    chart.data.labels = label;
+    chart.data.datasets.forEach((dataset) => {
+    	dataset.data = data;
+    });
+    chart.update();
+}
+
+function graphStock(data){
+	myChart = new Chart(
+			$(".chart-js"),
+			{"type":"line",
+				"data":{"labels":["January","February","March","April","May","June","July"],
+					"datasets":[
+						{"label":"Symbol",
+							"data":data,
+							"fill":false,
+							"borderColor":"rgb(75, 192, 192)",
+							"lineTension":0.1}
+							]},
+				"options":{
+					legend:{
+						display: false
+					},
+					scales:{
+						xAxes:[{
+							ticks: {
+                                display: true //set to false to remove x axis labels
+                            }
+						}]
+					}}
+			});
+
+	//myChart.data.datasets[0].data[1] = 75;
+	//myChart.update({easing: 'easeOutBounce'});
+}
+
 function handlePage(){
 	getSymbols(symList); //pull array of symbols and company names
+	graphStock(initData);
 	handleSearch();
+	handleTime();
 }
 $(handlePage);
